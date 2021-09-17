169f62d0bbbfc3eee66ac4f4b906e25a6119dcba
Yuliia Koba <yuliia.koba@nure.ua>
2021-09-17 22:48:05 +0300

    coloring squares after click

--- pages/simple_first/simpleFirst.js
+++ pages/simple_first/simpleFirst.js
@@ -1,3 +1,31 @@
+var rightPicture = [
+  [1, 1, 0, 0, 0, 0, 0, 1, 1, 1],
+  [0, 0, 0, 0, 0, 0, 0, 1, 1, 1],
+  [1, 1, 1, 0, 0, 0, 0, 0, 1, 1],
+  [1, 1, 1, 1, 1, 0, 1, 0, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1, 0, 1, 0],
+  [1, 0, 0, 0, 1, 1, 1, 1, 0, 0],
+  [0, 0, 0, 0, 1, 1, 1, 1, 1, 0],
+  [0, 0, 0, 0, 1, 1, 1, 1, 1, 1],
+  [0, 0, 0, 0, 0, 0, 0, 1, 1, 1],
+  [0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+];
+
+var currentPicture = [
+  [null, null, null, null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null, null, null, null],
+  [null, null, null, null, null, null, null, null, null, null],
+];
+
+var coordinates = [];
+
 function draw() {
   var canvas = document.getElementById('nanogram');
   if (canvas.getContext) {
@@ -36,8 +64,6 @@ function draw() {
     var x = width;
     var y = height;
 
-    console.log(x, y);
-
     for (var i = 0; i < columns.length; i++) {
       for (var j = columns[i].length - 1; j > -1; j--) {
         ctx.fillStyle = 'rgb(189,189,189)';
@@ -70,8 +96,10 @@ function draw() {
     y = height + 5;
 
     for (var i = 0; i < rows.length; i++) {
+      coordinates[i] = [];
       for (var j = 0; j < columns.length; j++) {
         ctx.strokeRect(x, y, 30, 30);
+        coordinates[i][j] = { x, y };
         x += 30;
       }
       x = width - 5;
@@ -89,4 +117,38 @@ function getMax(array) {
       30 +
     20
   );
+}
+
+function clickSquare() {
+  var canvas = document.getElementById('nanogram');
+  if (canvas.getContext) {
+    var ctx = canvas.getContext('2d');
+    var coordinate = findCoordinate(event.layerX, event.layerY);
+    console.warn(coordinate);
+    ctx.fillRect(coordinate.x, coordinate.y, 30, 30);
+  }
+}
+
+function findCoordinate(x, y) {
+  var i;
+
+  for (i = 0; i < coordinates.length; i++) {
+    if (y < coordinates[i][0].y) {
+      break;
+    }
+  }
+
+  i -= 1;
+
+  var j;
+
+  for (j = 0; j < coordinates[i].length; j++) {
+    if (x < coordinates[i][j].x) {
+      break;
+    }
+  }
+
+  j -= 1;
+
+  return { x: coordinates[i][j].x, y: coordinates[i][j].y, i, j };
 }
