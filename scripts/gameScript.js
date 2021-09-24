594aae172cfe13930c806bee94009fb2c3c01517
Yuliia Koba <yuliia.koba@nure.ua>
2021-09-24 18:18:27 +0300

    add style to the nonogram canvas

--- scripts/gameScript.js
+++ scripts/gameScript.js
@@ -455,72 +455,77 @@ var currentPicture = [
 var coordinates = [];
 
 function draw() {
-  var canvas = document.getElementById('nonogram');
-  canvas.height = level.height;
-  canvas.width = level.width;
-  if (canvas.getContext) {
-    var ctx = canvas.getContext('2d');
+  let myFont = new FontFace(
+    'SourceCodePro',
+    'url(https://fonts.gstatic.com/s/sourcecodepro/v14/HI_XiYsKILxRpg3hIP6sJ7fM7Pqt8srDs-cq.woff2)'
+  );
+  myFont.load().then((font) => {
+    document.fonts.add(font);
+    var canvas = document.getElementById('nonogram');
+    canvas.height = level.height;
+    canvas.width = level.width;
+    if (canvas.getContext) {
+      var ctx = canvas.getContext('2d');
+      ctx.strokeStyle = 'rgb(31, 37, 77)';
+      ctx.font = 'bold 22px SourceCodePro';
 
-    ctx.font = '30px serif';
+      var height = getMax(columns);
+      var width = getMax(rows);
 
-    var height = getMax(columns);
-    var width = getMax(rows);
+      var x = width;
+      var y = height;
 
-    var x = width;
-    var y = height;
+      for (var i = 0; i < columns.length; i++) {
+        for (var j = columns[i].length - 1; j > -1; j--) {
+          ctx.fillStyle = 'rgb(173,216,230)';
+          ctx.fillRect(x - 5, y - 30, 30, 35);
+          ctx.fillStyle = 'rgb(75,135,184)';
+          if (columns[i][j] > 9) {
+            ctx.fillText(columns[i][j], x - 3, y - 5);
+          } else {
+            ctx.fillText(columns[i][j], x + 2, y - 5);
+          }
 
-    for (var i = 0; i < columns.length; i++) {
-      for (var j = columns[i].length - 1; j > -1; j--) {
-        ctx.fillStyle = 'rgb(189,189,189)';
-        ctx.fillRect(x - 5, y - 30, 30, 35);
-        ctx.fillStyle = 'rgb(0,0,0)';
-        if (columns[i][j] > 9) {
-          ctx.fillText(columns[i][j], x - 7, y);
-        } else {
-          ctx.fillText(columns[i][j], x, y);
+          y -= 30;
         }
-
-        y -= 30;
-      }
-      y = height;
-      x += 30;
-    }
-
-    x = width - 30;
-    y += 30;
-
-    for (var i = 0; i < rows.length; i++) {
-      for (var j = rows[i].length - 1; j > -1; j--) {
-        ctx.fillStyle = 'rgb(189,189,189)';
-        ctx.fillRect(x - 5, y - 30, 30, 35);
-        ctx.fillStyle = 'rgb(0,0,0)';
-        if (rows[i][j] > 9) {
-          ctx.fillText(rows[i][j], x - 7, y);
-        } else {
-          ctx.fillText(rows[i][j], x, y);
-        }
-        x -= 30;
-      }
-      x = width - 30;
-      y += 30;
-    }
-
-    x = width - 5;
-    y = height + 5;
-
-    for (var i = 0; i < rows.length; i++) {
-      coordinates[i] = [];
-      for (var j = 0; j < columns.length; j++) {
-        ctx.strokeRect(x, y, 30, 30);
-        coordinates[i][j] = { x, y };
+        y = height;
         x += 30;
       }
-      x = width - 5;
-      y += 30;
-    }
-  }
 
-  console.log(coordinates);
+      x = width - 30;
+      y += 30;
+
+      for (var i = 0; i < rows.length; i++) {
+        for (var j = rows[i].length - 1; j > -1; j--) {
+          ctx.fillStyle = 'rgb(173,216,230)';
+          ctx.fillRect(x - 5, y - 30, 30, 35);
+          ctx.fillStyle = 'rgb(75,135,184)';
+          if (rows[i][j] > 9) {
+            ctx.fillText(rows[i][j], x - 3, y - 5);
+          } else {
+            ctx.fillText(rows[i][j], x + 2, y - 5);
+          }
+          x -= 30;
+        }
+        x = width - 30;
+        y += 30;
+      }
+
+      x = width - 5;
+      y = height + 5;
+
+      for (var i = 0; i < rows.length; i++) {
+        coordinates[i] = [];
+        for (var j = 0; j < columns.length; j++) {
+          ctx.strokeRect(x, y, 30, 30);
+          coordinates[i][j] = { x, y };
+          x += 30;
+        }
+        x = width - 5;
+        y += 30;
+      }
+    }
+  });
 }
 
 function getMax(array) {
@@ -581,8 +586,6 @@ function findCoordinate(x, y) {
 
 function drawSquare(ctx, coordinate) {
   var current = currentPicture[coordinate.i][coordinate.j];
-  console.log(coordinate);
-  console.warn(current);
   if (current === null) {
     drawBlackSquare(ctx, coordinate);
   } else if (current == 1) {
@@ -593,7 +596,7 @@ function drawSquare(ctx, coordinate) {
 }
 
 function drawBlackSquare(ctx, coordinate) {
-  ctx.fillStyle = 'rgb(0,0,0)';
+  ctx.fillStyle = 'rgb(75,135,184)';
   ctx.fillRect(coordinate.x, coordinate.y, 30, 30);
   ctx.fillStyle = 'rgb(255,255,255)';
   ctx.strokeRect(coordinate.x, coordinate.y, 30, 30);
@@ -603,7 +606,7 @@ function drawBlackSquare(ctx, coordinate) {
 function drawWhiteSquare(ctx, coordinate) {
   ctx.fillStyle = 'rgb(255,255,255)';
   ctx.fillRect(coordinate.x, coordinate.y, 30, 30);
-  ctx.fillStyle = 'rgb(0,0,0)';
+  ctx.fillStyle = 'rgb(75,135,184)';
   ctx.strokeRect(coordinate.x, coordinate.y, 30, 30);
   currentPicture[coordinate.i][coordinate.j] = null;
 }
@@ -611,13 +614,15 @@ function drawWhiteSquare(ctx, coordinate) {
 function drawBlock(ctx, coordinate) {
   ctx.fillStyle = 'rgb(255,255,255)';
   ctx.fillRect(coordinate.x, coordinate.y, 30, 30);
-  ctx.fillStyle = 'rgb(0,0,0)';
+  ctx.fillStyle = 'rgb(75,135,184)';
   ctx.beginPath();
+  ctx.lineWidth = 2;
   ctx.moveTo(coordinate.x, coordinate.y);
   ctx.lineTo(coordinate.x + 30, coordinate.y + 30);
   ctx.moveTo(coordinate.x, coordinate.y + 30);
   ctx.lineTo(coordinate.x + 30, coordinate.y);
   ctx.stroke();
+  ctx.lineWidth = 1;
   ctx.strokeRect(coordinate.x, coordinate.y, 30, 30);
   currentPicture[coordinate.i][coordinate.j] = 0;
 }
